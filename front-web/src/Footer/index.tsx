2c609924c8c5f2b0f8009a47dfbed45b93a8c484
insanniity <cleydson.cg@live.com>
2021-01-07 16:40:36 -0300

    Add Routes and StepsHeader

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -8,9 +8,9 @@ function Footer(){
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento semana DevSuperior
             <div className="footer-icons">
-                <a href="https://www.youtube.com.br/" target="_blank"><YoutubeIcon /></a>
-                <a href="https://www.linkedin.com/" target="_blank"><LinkedinIcon /></a>
-                <a href="https://www.instagram.com/" target="_blank"><InstagramIcon /></a>
+                <a href="https://www.youtube.com.br/" target="_new"><YoutubeIcon /></a>
+                <a href="https://www.linkedin.com/" target="_new"><LinkedinIcon /></a>
+                <a href="https://www.instagram.com/" target="_new"><InstagramIcon /></a>
             </div>
         </footer>
     )
